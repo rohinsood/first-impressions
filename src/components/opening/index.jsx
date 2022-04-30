eb1bc581745a42bcf721ea7bfaa560b8f4f0cc4f
Rohin Sood <rs.rohinsood@gmail.com>
2022-04-30 01:15:21 -0700

    BETTER COLORS 😈

--- src/components/opening/index.jsx
+++ src/components/opening/index.jsx
@@ -44,18 +44,18 @@ const Opening = ( {boards} ) => {
             <div className="highlight flex flex-row w-96 h-20 items-center justify-center space-x-6 rounded-tr-lg rounded-bl-lg">
                 
                 {/* sets the bottom limit to 0 and decreases move num by 1 when clicked */}
-                <button disabled={dis(0)} onClick={() => setMoveNum(moveNum-1)} className={String("bg-slate-200 border-2 border-black" + formatting)} data-tooltip-target="tooltip-default">
+                <button disabled={dis(0)} onClick={() => setMoveNum(moveNum-1)} className={formatting} data-tooltip-target="tooltip-default">
                     <TiArrowLeftThick size="36px" color="black"/>
                 </button>
 
                 {/* displays the current move number */}
-                <button  onClick={() => setMoveNum(0)} className="move-button border-2 border-white scale-95" >
+                <button  onClick={() => setMoveNum(0)} className="move-button bg-transparent scale-95" >
                     <TiArrowRepeat size="36px" color="black" />
                 </button>
 
 
                 {/* sets the top limit to the max number of moves (aka length of boards) and increases move num by 1 when clicked */}
-                <button disabled={dis(((boards[1].length - 1)))} onClick={() => setMoveNum(moveNum+1)} className={"bg-slate-200 border-2 border-black" + formatting} >
+                <button disabled={dis(((boards[1].length - 1)))} onClick={() => setMoveNum(moveNum+1)} className={formatting} >
                     <TiArrowRightThick size="36px" color="black"/>
                 </button>
             </div>
